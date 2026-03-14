5f851581d4876bc6f21c92bee32a9610cf2dcb8c
Bigwillie001 <davidtestimony183@gmail.com>
2026-03-14 05:21:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function copyCA(){
-  const ca = document.getElementById("contract").innerText;
-  navigator.clipboard.writeText(ca);
-  alert("Contract address copied!");
+function copyCA() {
+
+const contract = document.getElementById("contract").innerText;
+
+navigator.clipboard.writeText(contract);
+
+const toast = document.getElementById("copy-toast");
+
+toast.classList.add("show");
+
+setTimeout(() => {
+toast.classList.remove("show");
+}, 2000);
+
 }
